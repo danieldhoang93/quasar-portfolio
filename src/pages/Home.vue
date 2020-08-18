a1e6e48b163dab8c6e23cb947fec25fef9081172
Daniel Hoang <danieldhoang93@gmail.com>
2020-08-18 08:47:18 -0400

    disabled instasham for now so i can make lamda functions

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -74,10 +74,10 @@
                         control-color="white"
                         >
                             <q-carousel-slide :name="1" img-src="~/assets/instasham.jpg">
-                                <a href="http://instasham.netlify.app" target="_blank">
+                                <a href="" target="_blank">
                                     <div class="absolute-bottom custom-caption shadow">
                                         <div class="text-h3">Instasham</div>
-                                        <div class="text-subtitle1">Instagram clone that allows you to use the camera or upload photos to the mainpage<span class="text-italic">your</span> business.</div>
+                                        <div class="text-subtitle1">Instagram clone that allows you to take photos or upload photos to the mainpage<span class="text-italic">your</span> business.</div>
                                     </div>
                                 </a>
                             </q-carousel-slide>
