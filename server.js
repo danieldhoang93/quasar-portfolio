0068fe4265e0d19ea4315bdb291364f875f4dae2
Daniel Hoang <danieldhoang93@gmail.com>
2020-10-05 23:12:35 -0400

    emails

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.post('/email', (req, res) => {
     var transport = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
-        secure: true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASSWORD
